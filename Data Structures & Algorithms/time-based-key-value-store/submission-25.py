8bd1656ebad277ac18433fff4760256ca61b0499
Mehdy <85435879+MehdyShinwari@users.noreply.github.com>
2026-09-09 17:15:42 +0200

    Add: time-based-key-value-store - submission-25

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-25.py
@@ -0,0 +1,30 @@
+class TimeMap:
+
+    def ub(self, arr, x):
+        l, r = -1, len(arr)-1
+        while l < r:
+            m = (l+r+1)//2
+            if arr[m] > x:
+                r = m-1
+            else:
+                l = m
+        return arr[l] if l >=0 else None
+
+    def __init__(self):
+        self.data = {}
+        self.data1 = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.data[timestamp] = {key: value}
+        self.data1.setdefault(key, []).append(timestamp)
+
+    def get(self, key: str, timestamp: int) -> str:
+        if timestamp in self.data and key in self.data[timestamp]:
+            return self.data[timestamp][key]
+        elif key in self.data1:
+            i = self.ub(self.data1[key], timestamp)
+            if type(i) == int:
+                return self.data[i][key]
+            elif timestamp > self.data1[key][-1]:
+                return self.data[self.data1[key][-1]][key]
+        return ""
