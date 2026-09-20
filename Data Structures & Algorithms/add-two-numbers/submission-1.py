059f4d9c9dedc1aca5aab95e3967885e75150f30
Mehdy <85435879+MehdyShinwari@users.noreply.github.com>
2026-09-20 22:17:46 +0200

    Add: add-two-numbers - submission-1

---
+++ Data Structures & Algorithms/add-two-numbers/submission-1.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+from collections import deque
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        cur = dummy
+        takeover = 0
+        while l1 or l2 or takeover:
+            comb = (l1.val if l1 else 0) + (l2.val if l2 else 0) + takeover
+            takeover = 0
+            if comb <10:
+                cur.val = comb 
+            else:
+                cur.val = comb - 10
+                takeover = 1
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+            if l1 or l2 or takeover:
+                cur.next = ListNode()
+                cur = cur.next
+        return dummy
